834124726354878ca6489bc631c4b8811b0875e4
Suralmk <surafelmelaku940@gmail.com>
2024-05-31 11:00:33 +0300

    api endpoint added

--- src/Core/config.js
+++ src/Core/config.js
@@ -5,5 +5,5 @@ axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 axios.defaults.withCredentials = true
 
 export const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/'
+  baseURL: 'https://dashboardsurafel.pythonanywhere.com/api/'
 })
