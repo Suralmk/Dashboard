dc7af23dfe9bb36ec1672f841a10deb2bc761e81
Suralmk <surafelmelaku940@gmail.com>
2024-06-04 09:52:53 +0300

    Activity updated

--- src/Components/Activity.jsx
+++ src/Components/Activity.jsx
@@ -4,7 +4,7 @@ const Activity = ({ activity, time }) => {
   return (
     <div className='flex flex-col items-start gap-[1px] g-full bg-white my-[13px]'>
       <div className='w-full text-[15px] text-dark-two'>{activity}</div>
-      <div className='w-full text-[13px] text-dark-three'>{time}jhhjv</div>
+      <div className='w-full text-[13px] text-dark-three'>{time}</div>
     </div>
   )
 }
